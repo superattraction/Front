ac577fc26ba4625c0c6685edb8b6d9f32bf224f7
yhp0308 <flash_hwan@naver.com>
2024-07-02 11:41:53 +0900

    Admin 페이지 Polygon 색상 변경

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -256,7 +256,7 @@ export default function AdminPage() {
             aria-hidden="true"
           >
             <div
-              className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#FF80B5] to-[#9089FC]"
+              className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#9089FC] to-[#9089FC]"
               style={{
                 clipPath:
                   "polygon(100% 38.5%, 82.6% 100%, 60.2% 37.7%, 52.4% 32.1%, 47.5% 41.8%, 45.2% 65.6%, 27.5% 23.4%, 0.1% 35.3%, 17.9% 0%, 27.7% 23.4%, 76.2% 2.5%, 74.2% 56%, 100% 38.5%)",
